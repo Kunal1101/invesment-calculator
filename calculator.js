f6a16a0150b63436dc4cba6f344fb62cbaacb147
Kunal Mishra <kunalmishra8127@gmail.com>
2025-05-22 14:12:19 +0530

    investment calculator

---
+++ calculator.js
@@ -0,0 +1,45 @@
+"use strict";
+const calculateInvestment = (data) => {
+    const { initialAmount, annualContibution, expectedReturn, duration } = data;
+    if (initialAmount < 0)
+        return "Initial amount must be more then 0";
+    if (duration <= 0)
+        return "No valid years";
+    if (expectedReturn <= 0)
+        return "Expected return must be more then 0";
+    let total = initialAmount;
+    let totalContribution = 0;
+    let totalInterestEarned = 0;
+    const annualResults = [];
+    for (let i = 0; i < duration; i++) {
+        total = total * (1 + expectedReturn);
+        totalInterestEarned = total - totalContribution - initialAmount;
+        totalContribution = totalContribution + annualContibution;
+        total = total + annualContibution;
+        annualResults.push({
+            year: `Year ${i + 1}`,
+            totalAmount: total,
+            totalInterestEarned,
+            totalContribution,
+        });
+    }
+    return annualResults;
+};
+const printResults = (results) => {
+    if (typeof results === "string")
+        return console.log(results);
+    for (const yearEndResults of results) {
+        console.log(yearEndResults.year);
+        console.log(`Total: ${yearEndResults.totalAmount.toFixed(0)}`);
+        console.log(`Total Contribution: ${yearEndResults.totalContribution.toFixed(0)}`);
+        console.log(`Total Interest Earned: ${yearEndResults.totalInterestEarned.toFixed(0)}`);
+    }
+};
+const investmentData = {
+    initialAmount: 5000,
+    annualContibution: 500,
+    expectedReturn: 0.08,
+    duration: 10,
+};
+const results = calculateInvestment(investmentData);
+printResults(results);
